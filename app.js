4a165cafb5f785f5059d1b1e3eafb54e9516dcf1
Jeancisterna <jean.cisterna.saavedra@gmail.com>
2022-10-12 00:25:25 -0300

    arreglo elimniar

--- app.js
+++ app.js
@@ -4,34 +4,37 @@ let contactos = [];
 
 function agregarContactos(){
 const contacto ={};
-contacto.id = prompt("ingresar id");
-contacto.nombre = prompt("ingresar nombre");
-contacto.apellido = prompt("ingresar apellido");
+contacto.id = prompt("ingresa id");
+contacto.nombre = prompt("ingresa nombre");
+contacto.apellido = prompt("ingresa apellido");
 contacto.telefono = prompt("ingresa telefono")
 contacto.region = prompt("ingrese region");
 contacto.ciudad = prompt("ingresa ciudad");
 contacto.direccion = prompt("ingresa direccion");
 
 contactos.push(contacto);
+return contactos;
 }
-function eliminarContacos(contacto){
-    contactos = contactos.filter((nombre => contacto === nombre));
-    
+function eliminarContactos(id){
+    contactos = contactos.filter(x => x.id != id);
+    return contactos;
 }
+
  function actualizarContactos(){
     const contacto ={};
 
-    contacto.id = prompt("ingresar id actualizar");
-    contacto.nombre = prompt("ingresar nombre actualizar");
-    contacto.apellido = prompt("ingresar apellido actualizar");
-    contacto.telefono = prompt("ingresa telefono actualizar")
-    contacto.region = prompt("ingrese region actualizar");
-    contacto.ciudad = prompt("ingresa ciudad actualizar");
-    contacto.direccion = prompt("ingresa direccion actualizar");
+    contacto.id = prompt("ingrese id para actualizar");
+    contacto.nombre = prompt("ingrese nombre para actualizar");
+    contacto.apellido = prompt("ingrese apellido para actualizar");
+    contacto.telefono = prompt("ingrese telefono para actualizar")
+    contacto.region = prompt("ingrese region para actualizar");
+    contacto.ciudad = prompt("ingresa ciudad para actualizar");
+    contacto.direccion = prompt("ingresa direccion para actualizar");
 
     for (let i = 0; i < contactos.length; i++){
         if (contacto.id === contactos[i].id) (contactos[i]= contacto)
     }
+    return contactos;
 }
 
 agregarContactos();
