cbc7047bb27ce7caa04f681696275af8acc9120b
Jeancisterna <jean.cisterna.saavedra@gmail.com>
2022-10-20 00:14:30 -0300

    ingreso funcion ordenar atributo

--- app.js
+++ app.js
@@ -11,10 +11,33 @@ contacto.telefono = prompt("ingresa telefono")
 contacto.region = prompt("ingrese region");
 contacto.ciudad = prompt("ingresa ciudad");
 contacto.direccion = prompt("ingresa direccion");
-
+contacto.order = prompt("ingrese el orden");
 contactos.push(contacto);
 return contactos;
 }
+//ordenar contactos con el atributo order
+function ordenar(arr){
+    
+    for(var i = 0; i < arr.length; i++){
+       
+      
+      for(var j = 0; j < ( arr.length - i -1 ); j++){
+         
+        
+        if(arr[j].order > arr[j+1].order){
+           
+          
+          var temp = arr[j]
+          arr[j] = arr[j + 1]
+          arr[j+1] = temp
+        }
+      }
+    }
+    
+    console.log(arr);
+   }
+
+
 // funcion para poder eliminar contactos
 function eliminarContactos(id){
     contactos = contactos.filter(x => x.id != id);
@@ -39,6 +62,7 @@ function eliminarContactos(id){
 }
 
 agregarContactos();
+ordenar(contactos);
 
 
 /*contactos = prompt("ingresar contacto a elminiar");
